fe7ef2c3b75ff690e8f940450ad604cf17f6142f
Shine-MoonStar <3977373683@qq.com>
2025-11-13 07:01:27 +0800

    Create list.lua

---
+++ data/list.lua
@@ -0,0 +1,24 @@
+return {
+    ["地牢英雄"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Dungeon%20Heroes-obfuscated.lua",
+    ["战争大亨"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/WarTycoon-obfuscated.lua",
+    ["排球传奇"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Volleyball%20Legend-obfuscated.lua",
+    ["超速射击"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Hypershot-obfuscated.lua",
+    ["偷饼干"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Steal%20Cookie-obfuscated.lua",
+    ["钓它"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Fish-obfuscated.lua",
+    ["黑暗欺骗[狩猎]"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Hunted-obfuscated.lua",
+    ["死铁轨"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Dead%20rail-obfuscated.lua",
+    ["砍伐树木"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Cut%20Trees-obfuscated.lua",
+    ["紧急"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Emden-obfuscated.lua",
+    ["餐厅大亨3"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Restaurant%20Tycoon%203-obfuscated.lua",
+    ["脑叶公司"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Lobotomy%20Corporation-obfuscated.lua",
+    ["零售大亨2"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Retail%20Tycoon%202-obfuscated.lua",
+    ["启示录"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Apocalypse-obfuscated.lua",
+    ["萨凡纳生活(草原生活)"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Savannah%20Life-obfuscated.lua",
+    ["森林中99夜"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/99%20Nights%20in%20the%20Forest-obfuscated.lua",
+    ["索纳利亚生活"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Creatures%20of%20Sonaria%20Survive%20Kaiju%20Animals-obfuscated.lua",
+    ["卡车rng"] = "https://raw.githubusercontent.com/Nova-Moxiaobai/SwordGuest/refs/heads/main/Free/Truck%20Rng-obfuscated.lua"
+--[[
+    [""] = "",
+    [""] = ""
+]]
+}
